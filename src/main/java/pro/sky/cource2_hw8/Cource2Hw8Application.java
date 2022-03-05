3c181fef77c9d0a0ef5f3d178fca6c598843eaa7
Антон <antonL1790@yandex.ru>
2022-03-06 01:55:09 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cource2_hw8/Cource2Hw8Application.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2_hw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cource2Hw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cource2Hw8Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/cource2_hw8/Cource2Hw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2_hw8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cource2Hw8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
